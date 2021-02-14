d13ed5a1fb83a9b729f784fd3a5cd58bd6d7d235
cactice <14835424+Cactice@users.noreply.github.com>
2021-02-14 14:02:07 +0900

    Add nvidia docker

--- unix/ubuntu/apt.sh
+++ unix/ubuntu/apt.sh
@@ -18,3 +18,8 @@ guake \
 sudo apt-get install \
 libsecret-1-0 libsecret-1-dev \
 fonts-firacode
+
+# Nvidia Docker
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
--- unix/ubuntu/docker.sh
+++ unix/ubuntu/docker.sh
@@ -3,3 +3,6 @@ sudo sh get-docker.sh
 sudo usermod -aG docker $USER
 newgrp docker
 docker login
+sudo apt-get update
+sudo apt-get install -y nvidia-docker2
+sudo systemctl restart docker
