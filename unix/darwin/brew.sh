1e382442d0d3713ccdcdb2b449db2fb02d762fc0
cactice <14835424+Cactice@users.noreply.github.com>
2021-02-28 11:32:16 +0900

    Add mac dir

---
+++ unix/darwin/brew.sh
@@ -0,0 +1,8 @@
+#!bin/sh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install homebrew/cask 
+brew install iterm2 --cask
+brew install google-japanese-ime --cask
+brew install docker
+brew install google-chrome --cask
+brew install visual-studio-code --cask
