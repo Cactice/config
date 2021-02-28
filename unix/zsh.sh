538ae57f85f2e75116f28769bdf31c2961f33b26
Yuya Kanai <14835424+Cactice@users.noreply.github.com>
2021-02-28 12:23:21 +0900

    Update zsh.sh

--- unix/zsh.sh
+++ unix/zsh.sh
@@ -2,3 +2,4 @@ chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+ln .zshrc ~/.zshrc
