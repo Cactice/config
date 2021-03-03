4a78e0b76a83cf9a75bd736d466392ccfbde04b7
cactice <14835424+Cactice@users.noreply.github.com>
2021-03-03 09:56:39 +0900

    Update brew.sh

--- unix/darwin/brew.sh
+++ unix/darwin/brew.sh
@@ -2,7 +2,6 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install homebrew/cask 
 brew install iterm2 --cask
-brew install google-japanese-ime --cask
 brew install docker
 brew install docker --cask
 brew install google-chrome --cask
